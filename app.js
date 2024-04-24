c9505ae00ff5ff17c7430d8fccd6c021a5641baf
yasir-integry <54898550+yasir-integry@users.noreply.github.com>
2024-04-24 10:33:42 +0500

    Added endpoint initial code.

---
+++ app.js
@@ -0,0 +1,33 @@
+const generate = (str) =>
+  new Uint8Array(
+    [...unescape(encodeURIComponent(str))].map((c) => c.charCodeAt(0))
+  );
+
+app.get("/integry-params", function (req, res) {
+  const appSecret = "123";
+  const appKey = "345";
+  const userId = "yasir@integry.io";
+
+  const message = generate(userId);
+  const key = generate(appSecret);
+
+  const cryptoKey = await crypto.subtle.importKey(
+    "raw",
+    key,
+    { name: "HMAC", hash: "SHA-256" },
+    true,
+    ["sign"]
+  );
+
+  const signed = await crypto.subtle.sign("HMAC", cryptoKey, message);
+
+  const hash = [...new Uint8Array(signed)]
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  res.json({
+    hash,
+    appKey,
+    userId,
+  });
+});
